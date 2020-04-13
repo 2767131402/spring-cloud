64f2cbfa543892b3951540a9430b3b77fe56d08c
lizhenjiang <lizhenjiang@bochtec.com>
2020-04-13 14:57:59 +0800

    Initial commit

---
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/test/Test.java
@@ -0,0 +1,11 @@
+package com.funtl.hello.spring.cloud.web.admin.feign.test;
+
+/**
+ * @author zhenglei
+ * @version 1.0.0
+ * @ClassName Test.java
+ * @Description TODO
+ * @createTime 2019年10月30日
+ */
+public class Test {
+}
