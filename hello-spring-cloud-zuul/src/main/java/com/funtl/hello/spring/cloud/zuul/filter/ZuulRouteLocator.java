2948297ca715e40a81ec12ae218fcbbbf407d9b8
zhenglei <ii_zhenglei@163.com>
2020-07-30 18:40:29 +0800

    动态刷新路由

--- hello-spring-cloud-zuul/src/main/java/com/funtl/hello/spring/cloud/zuul/filter/ZuulRouteLocator.java
+++ hello-spring-cloud-zuul/src/main/java/com/funtl/hello/spring/cloud/zuul/filter/ZuulRouteLocator.java
@@ -1,6 +1,7 @@
 package com.funtl.hello.spring.cloud.zuul.filter;
 
 import org.springframework.cloud.netflix.zuul.filters.RefreshableRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.Route;
 import org.springframework.cloud.netflix.zuul.filters.SimpleRouteLocator;
 import org.springframework.stereotype.Component;
 import org.apache.commons.lang.StringUtils;
@@ -17,6 +18,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
 /**
  * Created by W2G on 2019/4/22.
  * 自定义动态路由定位器
@@ -26,6 +28,7 @@ import java.util.Map;
 public class ZuulRouteLocator extends SimpleRouteLocator implements RefreshableRouteLocator {
     public final static Logger logger = LoggerFactory.getLogger(ZuulRouteLocator.class);
 
+    private final static String DEFAULT_PATH_SEPARATOR = "/";
     private JdbcTemplate jdbcTemplate;
     private ZuulProperties properties;
 
@@ -34,7 +37,7 @@ public class ZuulRouteLocator extends SimpleRouteLocator implements RefreshableR
         super(server.getServlet().getContextPath(), properties);
         this.properties = properties;
         this.jdbcTemplate = jdbcTemplate;
-        logger.info("servletPath:{}",server.getServlet().getContextPath());
+        logger.info("servletPath:{}", server.getServlet().getContextPath());
     }
 
 
@@ -44,8 +47,43 @@ public class ZuulRouteLocator extends SimpleRouteLocator implements RefreshableR
     }
 
     /**
+     * 这个方法在网关产品中也很重要，可以根据实际路径匹配到Route来进行业务逻辑的操作，进行一些加工
+     *
+     * @param path
+     * @return
+     */
+    @Override
+    public Route getMatchingRoute(String path) {
+        return super.getMatchingRoute(path);
+    }
+
+    @Override
+    protected Route getRoute(ZuulProperties.ZuulRoute route, String path) {
+        int index = route.getPath().indexOf("*") - 1;
+        if (index > 0) {
+            return super.getRoute(route, path);
+        }
+
+        if (route == null) {
+            return null;
+        } else {
+            CustomZuulRoute customZuulRoute = jdbcTemplate.queryForObject("select * from gateway_zuul_route where id =? ", new BeanPropertyRowMapper<>(CustomZuulRoute.class), route.getId());
+
+            String prefix = customZuulRoute.getPrefix() + customZuulRoute.getVersion() + customZuulRoute.getResourceName();
+            String targetPath = route.getPath().replaceFirst(prefix, "");
+            Boolean retryable = this.properties.getRetryable();
+            if (route.getRetryable() != null) {
+                retryable = route.getRetryable();
+            }
+
+            return new Route(route.getId(), targetPath, route.getLocation(), prefix, retryable, route.isCustomSensitiveHeaders() ? route.getSensitiveHeaders() : null, route.isStripPrefix());
+        }
+    }
+
+    /**
      * 在simpleRouteLocator中具体就是在这儿定位路由信息的
      * 在这里重写方法后我们之后从数据库加载路由信息
+     *
      * @return
      */
     @Override
@@ -75,85 +113,95 @@ public class ZuulRouteLocator extends SimpleRouteLocator implements RefreshableR
         return values;
     }
 
-    @Cacheable(value = "locateRoutes",key = "RoutesFromDB",condition ="true")
-    public Map<String, ZuulProperties.ZuulRoute> locateRoutesFromDB(){
+    @Cacheable(value = "locateRoutes", key = "RoutesFromDB", condition = "true")
+    public Map<String, ZuulProperties.ZuulRoute> locateRoutesFromDB() {
         Map<String, ZuulProperties.ZuulRoute> routes = new LinkedHashMap<>();
         //创建动态路由配置类，用来获取所有的路由配置
-        List<CustomZuulRoute> results = jdbcTemplate.query("select * from gateway_zuul_route where enabled =1 ",new BeanPropertyRowMapper<>(CustomZuulRoute.class));
+        List<CustomZuulRoute> results = jdbcTemplate.query("select * from gateway_zuul_route where enabled =1 ", new BeanPropertyRowMapper<>(CustomZuulRoute.class));
 
         for (CustomZuulRoute result : results) {
-            if(StringUtils.isBlank(result.getPath())
-                    || (StringUtils.isBlank(result.serviceId) && StringUtils.isBlank(result.getUrl()))){
+            if (StringUtils.isBlank(result.getPath())
+                    || (StringUtils.isBlank(result.getServiceId()) && StringUtils.isBlank(result.getUrl()))) {
                 continue;
             }
 
             ZuulProperties.ZuulRoute zuulRoute = new ZuulProperties.ZuulRoute();
             try {
-                BeanUtils.copyProperties(result,zuulRoute);
+                BeanUtils.copyProperties(result, zuulRoute);
+                //拼接path
+                StringBuffer sb = new StringBuffer();
+                if (StringUtils.isNotBlank(result.getPrefix())) {
+                    sb.append(result.getPrefix());
+                }
+                if (StringUtils.isNotBlank(result.getVersion())) {
+                    sb.append(result.getVersion());
+                }
+                if (StringUtils.isNotBlank(result.getResourceName())) {
+                    sb.append(result.getResourceName());
+                }
+                String path = sb.toString() + result.getPath();
+                zuulRoute.setPath(path);
             } catch (Exception e) {
-                logger.error("load zuul route info from db has error",e);
+                logger.error("load zuul route info from db has error", e);
             }
-            routes.put(zuulRoute.getPath(),zuulRoute);
+            routes.put(zuulRoute.getPath(), zuulRoute);
         }
 
         return routes;
     }
 
 
-    public static class CustomZuulRoute {
-        private String id;
-        private String path;
-        private String serviceId;
-        private String url;
-        private boolean stripPrefix = true;
-        private Boolean retryable;
+    public static class CustomZuulRoute extends ZuulProperties.ZuulRoute {
+        private String prefix;
+        private String version;
+        private String resourceName;
 
-        public String getId() {
-            return id;
+        public String getPrefix() {
+            if (StringUtils.isBlank(prefix)) {
+                return "";
+            } else {
+                if (prefix.startsWith(DEFAULT_PATH_SEPARATOR)) {
+                    return prefix;
+                } else {
+                    return DEFAULT_PATH_SEPARATOR + prefix;
+                }
+            }
         }
 
-        public void setId(String id) {
-            this.id = id;
+        public void setPrefix(String prefix) {
+            this.prefix = prefix;
         }
 
-        public String getPath() {
-            return path;
+        public String getVersion() {
+            if (StringUtils.isBlank(version)) {
+                return "";
+            } else {
+                if (version.startsWith(DEFAULT_PATH_SEPARATOR)) {
+                    return version;
+                } else {
+                    return DEFAULT_PATH_SEPARATOR + version;
+                }
+            }
         }
 
-        public void setPath(String path) {
-            this.path = path;
+        public void setVersion(String version) {
+            this.version = version;
         }
 
-        public String getServiceId() {
-            return serviceId;
+        public String getResourceName() {
+            if (StringUtils.isBlank(resourceName)) {
+                return "";
+            } else {
+                if (resourceName.startsWith(DEFAULT_PATH_SEPARATOR)) {
+                    return resourceName;
+                } else {
+                    return DEFAULT_PATH_SEPARATOR + resourceName;
+                }
+            }
         }
 
-        public void setServiceId(String serviceId) {
-            this.serviceId = serviceId;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-
-        public boolean isStripPrefix() {
-            return stripPrefix;
-        }
-
-        public void setStripPrefix(boolean stripPrefix) {
-            this.stripPrefix = stripPrefix;
-        }
-
-        public Boolean getRetryable() {
-            return retryable;
-        }
-
-        public void setRetryable(Boolean retryable) {
-            this.retryable = retryable;
+        public void setResourceName(String resourceName) {
+            this.resourceName = resourceName;
         }
     }
 
--- hello-spring-cloud-zuul/src/main/resources/db/zuul.sql
+++ hello-spring-cloud-zuul/src/main/resources/db/zuul.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50717
  File Encoding         : 65001
 
- Date: 29/07/2020 18:47:34
+ Date: 30/07/2020 15:18:51
 */
 
 SET NAMES utf8mb4;
@@ -23,6 +23,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `gateway_zuul_route`;
 CREATE TABLE `gateway_zuul_route`  (
   `id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `prefix` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `version` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `resource_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `service_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
@@ -36,8 +39,8 @@ CREATE TABLE `gateway_zuul_route`  (
 -- ----------------------------
 -- Records of gateway_zuul_route
 -- ----------------------------
-INSERT INTO `gateway_zuul_route` VALUES ('1', '/api/c/**', 'hello-spring-cloud-service', NULL, 0, 1, 1, NULL);
-INSERT INTO `gateway_zuul_route` VALUES ('2', '/api/b/**', 'hello-spring-cloud-web-admin-feign', NULL, 0, 1, 1, NULL);
-INSERT INTO `gateway_zuul_route` VALUES ('3', '/api/a/**', 'hello-spring-cloud-web-admin-ribbon', NULL, 0, 1, 1, NULL);
+INSERT INTO `gateway_zuul_route` VALUES ('1', '/api', NULL, '/c', '/test', 'hello-spring-cloud-service', NULL, 0, 1, 1, NULL);
+INSERT INTO `gateway_zuul_route` VALUES ('2', '/api', NULL, '/b', '/hi', 'hello-spring-cloud-web-admin-feign', NULL, 0, 1, 1, NULL);
+INSERT INTO `gateway_zuul_route` VALUES ('3', '/api', NULL, '/a', '/hi', 'hello-spring-cloud-web-admin-ribbon', NULL, 0, 1, 1, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
